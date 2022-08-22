187307ea0f6b6bf0dffa1d6216e305f45f591ee8
Justin <justin.kiarie@student.moringaschool.com>
2022-08-22 10:05:27 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main");// Write your code here!
+element.remove()
+const newHeader = document.createElement("h1")
+document.body.appendChild(newHeader)
+newHeader.setAttribute('id','victory')
+
+newHeader.innerHTML="Justin is the champion"
